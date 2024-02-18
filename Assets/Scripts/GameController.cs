78d9b7830f1894b89fb8c338fa2a222bfb3ce3b8
Sravan Kairamkonda <35738653+SravanKairamkonda@users.noreply.github.com>
2024-02-18 20:07:11 +0000

    Matching the sequences

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,16 +6,24 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject nodeButton;
+    [SerializeField] private GameObject[] gameObjects;
+
+    private const char xValue = 'X';
+    private const char oValue = 'O';
+
+    private string tempName;
 
     private void Start()
     {
         GenerateBoard();
+        MatchThreeValue();
     }
 
     private void GenerateBoard()
     {
         int x = 0;
         int y = 0;
+        int z = 0;
 
         for (int i = 0; i <3; i++)
         {
@@ -25,12 +33,21 @@ public class GameController : MonoBehaviour
                 node.transform.SetParent(this.transform);
                 Vector2 pos = new Vector2(x, y);
                 node.GetComponent<RectTransform>().anchoredPosition = pos;
+                node.GetComponentInChildren<Text>().text = xValue.ToString();
                 x += 100;
             }
 
             y += 100;
             x = 0;
-            
+        }
+    }
+
+    private void MatchThreeValue()
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            tempName=gameObjects[i].transform.GetComponent<Text>().text=xValue.ToString();
+            Debug.Log(tempName);    
         }
     }
     
