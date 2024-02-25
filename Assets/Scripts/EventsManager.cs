4879bc8d99733f148cb9c0a8cd1005d144478f1b
Sravan Kairamkonda <35738653+SravanKairamkonda@users.noreply.github.com>
2024-02-25 10:14:18 +0000

    Took some notes and know the type of delegate
    Temp delegate
    Instant value check by delegate
    Action and Func are other built types delegates in System library

--- Assets/Scripts/EventsManager.cs
+++ Assets/Scripts/EventsManager.cs
@@ -7,10 +7,29 @@ public class EventsManager : MonoBehaviour
     public delegate void TestDelegate();
     private TestDelegate testDelegateFunction;
 
-    private void Start()
+    public delegate bool testCardValueDelegate(string value);
+    private testCardValueDelegate m_TestCardvalueFunction;
+
+    private string m_debugMessage;
+
+    private void Awake()
     {
-        testDelegateFunction = MyTestDelegateFunction;
+        // The temp function can't unsubcribe from the delegate since it does not have reference 
+        // so it better to create new fucntion and add it ot the delegate.
+        testDelegateFunction = delegate () { Debug.Log("Function TBA"); };
         testDelegateFunction();
+
+        testDelegateFunction = MyTestDelegateFunction;
+        testDelegateFunction+=SecondDelegateFunction;
+        testDelegateFunction();
+
+        m_TestCardvalueFunction = TestCardValue;
+        Debug.Log( m_TestCardvalueFunction("J").ToString());
+
+        // New methods can be added in future or test temporary
+        m_TestCardvalueFunction = (string s) => { return s == "K"; };
+        Debug.Log( m_TestCardvalueFunction("K").ToString());
+
     }
 
     private void MyTestDelegateFunction()
@@ -18,4 +37,22 @@ public class EventsManager : MonoBehaviour
         Debug.Log("My Degelate function");
     }
 
+    private void SecondDelegateFunction()
+    {
+        Debug.Log("Second Delegate Function");
+    }
+
+    private bool TestCardValue(string value) 
+    {
+        return value == "K";
+    }
+
+    /* Notes
+     There are simliar built types functionalities in "SYSTEM".
+     * Action, which does not return type and similar to delegate funcitonality. 
+       It can take parameters
+     * Func, Which has return Type and other functionalities from Action.
+     
+     
+     */
 }
